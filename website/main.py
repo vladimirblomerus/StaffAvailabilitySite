bdfc05d787c429e2c20d17cc7aff9da3b44bcc06
Vladimir Blomerus <vladimir.blomerus@gmail.com>
2024-08-20 16:40:25 +0200

    feat: Update website administration page, exchanging availability and messages for latest updates

--- website/main.py
+++ website/main.py
@@ -18,12 +18,12 @@ def administration():
             "Onsdag - Fredag: 8:00 - 12:00",
         ],
     }
-    availability = {
-        "message": "Stängt för dagen",
-        "datetime": "imorgon, 12:00",
+    updates = {
+        "message": "Jag vill informera er om att jag tyvärr måste hålla stängt imorgon på grund av sjukdom. Jag beklagar verkligen det besvär detta kan orsaka och hoppas på er förståelse under dessa omständigheter. Min förhoppning är att kunna återgå till ordinarie öppettider så snart som möjligt, och jag tackar för ert tålamod och er förståelse.",
+        "published_datetime": "2024-08-20 12:00",
     }
 
-    return render_template("/view-signs/administration.html", page_title="Administration", person=person, availability=availability, status_messages=[ "Lorem ipsum dolor sit amet.", "Ut enim ad minim veniam." ])
+    return render_template("/view-signs/administration.html", page_title="Administration", person=person, updates=updates)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
