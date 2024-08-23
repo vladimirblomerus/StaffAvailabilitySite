4a9f2026ba59bedabaa406b2477929853780a58e
Vladimir Blomerus <vladimir.blomerus@gmail.com>
2024-08-23 12:39:57 +0200

    refactor (QRCodeCreator): move generated QR directory into QRCodeCreator folder, remove unsed references

--- QRCodeCreator/qr_code_creator.py
+++ QRCodeCreator/qr_code_creator.py
@@ -1,9 +1,8 @@
 import os
-import pathlib
 import qrcode
 
 FILE_EXTENSION = ".jpg" # File extension for qr codes
-QR_CODE_FOLDER = "QR-codes" # Folder to store qr codes
+QR_CODE_FOLDER = "QRCodeCreator/QR-codes" # Folder to store qr codes
 
 if not os.path.exists(QR_CODE_FOLDER):
     os.makedirs(QR_CODE_FOLDER)
@@ -14,7 +13,6 @@ def generate_qr_code(data, file_name):
     type(img)
     img.save(f"{QR_CODE_FOLDER}/{file_name}{FILE_EXTENSION}")
 
-created_qr_code_names = [] # List to store names of created qr codes for printing
 def create_qr_codes():
     """Function to create qr codes"""
     print("Generate new QR code:")
@@ -25,7 +23,6 @@ def create_qr_codes():
     if qr_name == "":
         qr_name = qr_data
     generate_qr_code(qr_data, qr_name)
-    created_qr_code_names.append(qr_name)
     create_qr_codes()
 
 create_qr_codes()
